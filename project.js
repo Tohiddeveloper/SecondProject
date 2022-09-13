8e94d01df7da347566237853b4095826843daeca
tohid820 GitHub <tohidkhan896.tk@gmail.com>
2022-09-13 18:15:32 +0530

    first commit

---
+++ project.js
@@ -0,0 +1,15 @@
+function changeMode(){
+    let element = document.body;
+    element.classList.toggle('dark-mode')
+}
+function loadCoupon(){
+    document.getElementById('coupon').style.display="block";
+    document.getElementById('Maincontainer').style.opacity="0.1";
+    document.getElementById('brand').style.opacity="0.1";
+    document.getElementById('info').style.opacity="0.1";
+}
+
+const closeCoupon = () => {
+    document.getElementById('coupon').style.display="none";
+    document.getElementById('Maincontainer').style.opacity="1";
+}
